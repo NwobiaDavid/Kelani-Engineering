87213fb9ed84e7fdacc099429e16d7678268f2e4
Ronald Dosunmu <ronaldosunmu@gmail.com>
2023-12-22 10:13:29 +0100

    finish footer styles

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -20,8 +20,8 @@ const Footer = () => {
   return (
     <div className="w-screen h-screen relative p-[24px]">
           <img className="w-full h-full object-cover absolute top-0 right-0 left-0 bottom-0 -z-10" src="/assets/images/footer-bg.png" />
-          <div className="bg-white w-full h-full rounded-[9.6px] grid grid-cols-2 gap-[24px]">
-              <div className="h-full p-[56px] flex flex-col justify-center items-center relative">
+          <div className="bg-white w-full h-full rounded-[9.6px] flex flex-col overflow-y-scroll lg:overflow-hidden lg:grid grid-cols-2 lg:gap-[24px] lg:p-0 relative">
+              <div className="h-full p-[24px] lg:p-[56px] flex flex-col justify-center items-center lg:relative">
 
                 <div className="h-[50px] w-[150px] bg-gray-400 mb-[84px]">
 
@@ -32,9 +32,9 @@ const Footer = () => {
                   <p>SALES@6B47.COM</p>
                 </div>
 
-                <div className="flex absolute bottom-[46px]">
-                  <p className="mr-[100px] text-[#888A8B]">© 2023 6B47 REAL ESTATE INVESTORS AG</p>
-                  <p className="flex text-[#888A8B]">
+                <div className="flex flex-col lg:flex-row left-[56px] absolute hidden lg:block lg:bottom-[46px]">
+                  <p className="lg:mr-[100px] text-[#888A8B]">© 2023 6B47 REAL ESTATE INVESTORS AG</p>
+                  <p className="flex text-[#888A8B] flex-row">
                     <a className="mr-[23px] inline-block">Imprints</a>
                     <a className="">Legal</a>
                   </p>
@@ -42,8 +42,8 @@ const Footer = () => {
 
                 <div className="flex gap-[23px] mt-[40px]"><p>linkedin</p><p>google</p></div>
               </div>
-              <div className="h-full p-[56px] pb-[97px] flex flex-col justify-between">
-                  <p className="max-w-[80%] text-[17.25px]">If you want to book an appointment or find solutions, fill out the form below and our manager will contact you within a day</p>
+              <div className="h-full p-[24px] lg:p-[56px] pb-[48px] lg:pb-[97px] flex flex-col gap-[30px] lg:justify-between border-t border-t-[#D2DADF] lg:border-top-0">
+                  <p className="lg:max-w-[80%] text-[17.25px]">If you want to book an appointment or find solutions, fill out the form below and our manager will contact you within a day</p>
                   <form className="flex flex-col gap-[50px]">
                       <ContactUsInput register={register} name="name" placeholder="Name" value="" onChange={() => {}} />
                       <ContactUsInput register={register} name="email" placeholder="Email" value="" type="email" onChange={() => {}} />
@@ -53,6 +53,13 @@ const Footer = () => {
                   </form>
                       <button className="py-[12px] px-[32px] w-fit text-[#222] border border-[#222] rounded-full" type="submit">Send</button>
               </div>
+              <div className="flex flex-col lg:flex-row text-center  lg:hidden block px-[24px] pb-[20px]">
+                  <p className="lg:mr-[100px] text-[#888A8B]">© 2023 6B47 REAL ESTATE INVESTORS AG</p>
+                  <p className="flex text-[#888A8B] flex-row lg:flex-col justify-center text-center">
+                    <a className="mr-[23px] inline-block">Imprints</a>
+                    <a className="">Legal</a>
+                  </p>
+                </div>
 
           </div>
     </div>
