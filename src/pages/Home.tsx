2cedf0ebc63d5878897e278df8f0483dcf71caeb
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2024-01-04 00:42:42 +0100

    structure home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import AboutCompany from "../sections/AboutCompany";
+import Footer from "../sections/Footer";
+import Hero from "../sections/Hero";
+import SubsidiaryShowcase from "../sections/SubsidiaryShowcase";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <AboutCompany />
+      <SubsidiaryShowcase />
+      <SubsidiaryShowcase />
+      <SubsidiaryShowcase />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
