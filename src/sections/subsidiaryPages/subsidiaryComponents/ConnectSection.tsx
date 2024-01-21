d55d94e918cb0e670f69997c35be4bd93bbf6c0f
NwobiaDavid <dnwobia@gmail.com>
2024-01-21 07:53:54 +0100

    cleaned the customersection code

--- src/sections/subsidiaryPages/subsidiaryComponents/ConnectSection.tsx
+++ src/sections/subsidiaryPages/subsidiaryComponents/ConnectSection.tsx
@@ -6,6 +6,7 @@ const ConnectSection: React.FC = () => {
     return (
         <div className=' rounded-b-3xl  w-full h-[650px] lg:h-screen relative flex justify-center items-center text-white '>
             <img className="absolute text-center  " src="/assets/images/subsidiaryPagesImages/props/Circles.png" alt="" />
+            
             <div className='flex z-50 relative items-center justify-center flex-col h-full w-[90%] lg:w-[35%] text-center '>
                 <h1 className=' text-4xl lg:text-5xl uppercase font-semibold lg:font-bold'>Connect with Us Today</h1>
                 <p className='text-xl opacity-85 my-10'>Have questions or ready to explore how our solutions can benefit you? Reach out to us! Our dedicated team is here to assist. Contact us for personalized assistance and let's embark on a journey towards your success.</p>
@@ -13,6 +14,7 @@ const ConnectSection: React.FC = () => {
                     <Link to={"#"} className="w-full" >GET CONNECTED </Link><IoIosArrowRoundForward size={30} />
                 </div>
             </div>
+
         </div>
     )
 }
--- src/sections/subsidiaryPages/subsidiaryComponents/CustomerSection.tsx
+++ src/sections/subsidiaryPages/subsidiaryComponents/CustomerSection.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { motion } from 'framer-motion';
 import Marquee from "react-fast-marquee";
 
 
@@ -35,23 +34,26 @@ const CustomerSection: React.FC = () => {
     <div className="w-full  h-[500px] lg:h-fit ">
         <div className="lg:h-[500px] z-50 relative h-[200px]">
           <h1 className='text-white opacity-60 mb-6 text-center font-semibold text-xs lg:text-base lg:mb-10 '>CUSTOMERS</h1>
-                <div className='flex w-full relative justify-center items-center'>
+
+          <div className='flex w-full relative justify-center items-center'>
           <img src="assets/images/subsidiaryPagesImages/props/marquee-line-left.png" alt=""  className=' invisible lg:visible left-[8.3rem] top-[14.2rem]' />
 
             <div className=' w-[500px] 3xl:w-[55%] lg:w-[88%] flex-col relative flex justify-center items-center '>
                 <Marquee pauseOnHover={true} className='border  border-opacity-40 border-white rounded-xl ' >
                   {cards.map((item, index)=>(
+
                       <div key={index} className='m-0 bg-black p-2 w-[150px] h-[100px] md:w-[250px] md:h-[170px] lg:h-[190px] lg:w-[260px] 3xl:h-[200px] 3xl:w-[270px] flex justify-center items-center border-white text-white border border-y-0 border-opacity-30 '>
                               <div className=' flex justify-center items-center'>
                                   <img src={item.img} className=' w-[45px] md:w-full ' alt="" />
                               </div>
                       </div>
+
                   ))}
                 </Marquee>
             </div>
 
-            <img src="assets/images/subsidiaryPagesImages/props/marquee-line-right.png" alt="" className='invisible lg:visible right-[8.3rem] top-[14.2rem]' />
-                </div>
+            <img src="assets/images/subsidiaryPagesImages/props/marquee-line-right.png" alt="" className='invisible lg:visible right-[8.3rem] top-[14.2rem]' /> 
+          </div>
         </div>
     </div>
   );
