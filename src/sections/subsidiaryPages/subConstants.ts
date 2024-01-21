d253cb47fd5b8f341349d303f05df0eea49db56a
NwobiaDavid <dnwobia@gmail.com>
2024-01-21 08:09:18 +0100

    tured the features page into a component

---
+++ src/sections/subsidiaryPages/subConstants.ts
@@ -0,0 +1,34 @@
+// for the features page
+
+// page 1 ----------------------------------------
+
+export const features_content = [
+    {
+        img: 'assets/images/subsidiaryPagesImages/props/save-energy 1.png',
+        img2:  'assets/images/subsidiaryPagesImages/props/Gap.png',
+        head: "Specialized Talent Acquisition & Development",
+        desc: "Timely and comprehensive data for better decision-making, reliability,resilience, and capacity management",
+        colour: "text-purple-400"
+    },
+    {
+        img: 'assets/images/subsidiaryPagesImages/props/save-energy 1-1.png',
+        img2: 'assets/images/subsidiaryPagesImages/props/Gap.png',
+        head: "Efficient HRMS  for Operations",
+        desc: "Timely and comprehensive data for better decision-making, reliability,resilience, and capacity management",
+        colour: "text-purple-400"
+    },
+    {
+        img: 'assets/images/subsidiaryPagesImages/props/save-energy 1-2.png',
+        img2: 'assets/images/subsidiaryPagesImages/props/Gap.png',
+        head: "Strategic Outsourcing for Engineering Tasks",
+        desc: "Timely and comprehensive data for better decision-making, reliability,resilience, and capacity management",
+        colour: "text-purple-400"
+    },
+    {
+        img: 'assets/images/subsidiaryPagesImages/props/save-energy 1-3.png',
+        img2: 'assets/images/subsidiaryPagesImages/props/Gap.png',
+        head: "Cost Savings and Flexibility through Outsourcing",
+        desc: "Timely and comprehensive data for better decision-making, reliability, resilience, and capacity management",
+        colour: "text-purple-400"
+    },
+]
--- src/sections/subsidiaryPages/subsidiaryComponents/Features.tsx
+++ src/sections/subsidiaryPages/subsidiaryComponents/Features.tsx
@@ -1,39 +1,16 @@
 import React from 'react'
+import { features_content } from '../subConstants';
 
-interface Contents {
+interface features_content {
     img: string;
     img2: string;
     head: string;
     desc: string;
+    colour: string;
   }
 
 const Features: React.FC = () => {
-    const contents: Contents[]=[
-        {
-            img: 'assets/images/subsidiaryPagesImages/props/save-energy 1.png',
-            img2:  'assets/images/subsidiaryPagesImages/props/Gap.png',
-            head: "Specialized Talent Acquisition & Development",
-            desc: "Timely and comprehensive data for better decision-making, reliability,resilience, and capacity management"
-        },
-        {
-            img: 'assets/images/subsidiaryPagesImages/props/save-energy 1-1.png',
-            img2: 'assets/images/subsidiaryPagesImages/props/Gap.png',
-            head: "Efficient HRMS  for Operations",
-            desc: "Timely and comprehensive data for better decision-making, reliability,resilience, and capacity management"
-        },
-        {
-            img: 'assets/images/subsidiaryPagesImages/props/save-energy 1-2.png',
-            img2: 'assets/images/subsidiaryPagesImages/props/Gap.png',
-            head: "Strategic Outsourcing for Engineering Tasks",
-            desc: "Timely and comprehensive data for better decision-making, reliability,resilience, and capacity management"
-        },
-        {
-            img: 'assets/images/subsidiaryPagesImages/props/save-energy 1-3.png',
-            img2: 'assets/images/subsidiaryPagesImages/props/Gap.png',
-            head: "Cost Savings and Flexibility through Outsourcing",
-            desc: "Timely and comprehensive data for better decision-making, reliability, resilience, and capacity management"
-        },
-    ]
+    
   return (
     <div className='z-10 lg:h-fit text-white'>
 
@@ -43,13 +20,13 @@ const Features: React.FC = () => {
             </div>
             
             <div className='flex lg:p-4 w-full lg:w-[75%] flex-col lg:flex-row justify-between items-center'>
-            {contents.map((item, index)=>(
+            {features_content.map((item, index)=>(
 
                 <div className="w-full flex-col flex justify-center items-center ">
                     <div key={index} className='flex py-4 w-full md:w-[270px] mb-10 flex-col justify-center items-center'>
                         <img src={item.img} className='pb-4 md:p-0 w-[10rem] md:w-[120px] opacity-90 ' alt={item.head} />
                     <img src={item.img2} className='w-[100vw] lg:hidden' alt="" />
-                        <h1 className='text-purple-400 px-4 text-xl_2 font-semibold opacity-90 md:text-2xl mt-3 mb-3 lg:mb-6 '>{item.head}</h1>
+                        <h1 className={`${item.colour} px-4 text-xl_2 font-semibold opacity-90 md:text-2xl mt-3 mb-3 lg:mb-6`} >{item.head}</h1>
                         <p className='text-sm opacity-80 px-4 font-medium '>{item.desc}</p>
                     </div>
                 </div>
