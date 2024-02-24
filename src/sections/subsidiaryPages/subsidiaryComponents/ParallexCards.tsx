a75eb6946ccdc7030f99d6839cccf1809c2db11e
NwobiaDavid <dnwobia@gmail.com>
2024-02-24 10:26:25 +0100

    fixed the parallax

--- src/sections/subsidiaryPages/subsidiaryComponents/ParallexCards.tsx
+++ src/sections/subsidiaryPages/subsidiaryComponents/ParallexCards.tsx
@@ -22,7 +22,7 @@ interface ParallexProps {
 }
 
 
-const ParallaxCards: React.FC<{ data: ParallexProps  }> = ({ data }) => {
+const ParallaxCards: React.FC<{ data: ParallexProps }> = ({ data }) => {
 
 
   // const [isHovered, setIsHovered] = useState(false);
@@ -77,164 +77,164 @@ const ParallaxCards: React.FC<{ data: ParallexProps  }> = ({ data }) => {
   return (
     <>
 
-<div className="relative h-[400vh] z-30  flex-col text-white w-full flex">
-    
-    <div className="main sticky  top-0 h-[100vh]  items-center  py-10 flex-col text-white w-full flex">
+      <div className="relative h-[400vh] z-30  flex-col text-white w-full flex">
 
-              <div >
-                <div className="flex flex-col  uppercase justify-center items-center">
-                  <div className="  w-[90%] text-center relative  ">
-                    <h3 className=" opacity-80 tracking-wider lg:font-semibold mb-2 lg:mb-6  ">
-                      why kelani
-                    </h3>
-                    <h1 className="lg:text-5xl text-4xl text-center font-semibold ">
-                      Personalized Service for You
-                    </h1>
+        <div className="main sticky  top-0 h-[100vh]  items-center  py-10 flex-col text-white w-full flex">
+
+          <div >
+            <div className="flex flex-col  uppercase justify-center items-center">
+              <div className="  w-[90%] text-center relative  ">
+                <h3 className=" opacity-80 tracking-wider lg:font-semibold mb-2 lg:mb-6  ">
+                  why kelani
+                </h3>
+                <h1 className="lg:text-5xl text-4xl text-center font-semibold ">
+                  Personalized Service for You
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex h-full relative overflow-x-hidden mt-[20px]  w-full ">
+
+            <div className="w-[15%] h-full justify-center items-center hidden lg:flex flex-col ">
+              <div className="w-[50%] flex flex-col items-center justify-center text-center ">
+                <span className="text-xl opacity-70 font-semibold ">
+                  Keep Scrolling{" "}
+                </span>
+
+                <motion.div
+                  className="relative  py-6 flex justify-center " >
+                  <motion.img
+                    className=" absolute h-[70px] opacity-80 "
+                    src="assets/images/subsidiaryPagesImages/props/Vector.png"
+                    alt=""
+                    animate={{ y: [-10, 10, -10], transition: { duration: 2, repeat: Infinity } }}
+                  />
+                  <motion.img
+                    className="my-3 h-[100px] "
+                    src="assets/images/subsidiaryPagesImages/props/Vector (1).png"
+                    alt=""
+                    animate={{ scale: [1.2, 1, 1.2], transition: { duration: 3, repeat: Infinity } }}
+                  />
+                </motion.div>
+
+                <span className="opacity-80 ">To Discover</span>
+              </div>
+            </div>
+
+            <div className=" z-50 h-full w-[85%] relative">
+              <div className=" flex ml-[4.5%]  w-full h-full ">
+
+
+
+                <div style={{ background: `url(${data.data[0].img2})`, transform: 'rotate(180deg)', backgroundSize: 'cover', }}
+                  className="w-[65%] 3xl:w-[55%] border-white border rounded-[20px] 3xl:h-[50%]  lg:h-[80%] absolute 3xl:bottom-[20%] bottom-[10%] pl-[100px] overflow-hidden card-1 ">
+
+                  <div style={{ transform: 'rotate(-180deg)' }} className="h-full flex justify-center items-center w-full" >
+                    <div className="h-[100%] pt-[15px] left-0 w-[10%]  flex flex-col items-center">
+                      <div className="w-full h-[20%] flex items-center justify-center">
+                        <img
+                          className="w-[60px] rotate-90 relative opacity-0 top-[100px] left-[100px] top-left-image"
+                          src={data.data[0].img}
+                        />
+                      </div>
+                      <div className="h-[280px]  w-full flex justify-center overflow-hidden">
+                        <div className="w-[1.5px] bg-[#F4F4F4] h-full relative top-[100%] line-extend"></div>
+                      </div>
+                    </div>
+
+                    <div className="w-[85%] h-full flex py-[65px] ">
+                      <div className=" w-[30%] ">
+                        <h2 className="text-[44px]  pl-[5%] text-white leading-[50.2px] title">
+                          {data.data[0].content.head}
+                        </h2>
+                        <p className="mt-[16px]  pl-[5%] ">
+                          {data.data[0].content.text}
+                        </p>
+                        <button className=" pl-[5%] text-white flex gap-[13px] mt-[44px] items-center">
+                          Learn More
+                          <img src="/assets/images/right-arrow.svg" />
+                        </button>
+                      </div>
+                      <img
+                        className="w-[70%] ml-10 object-cover"
+                        src={data.data[0].img3}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+
+                <div style={{ background: `url(${data.data[0].img2})`, transform: 'rotate(180deg)', backgroundSize: 'cover', }}
+                  className="w-[65%] 3xl:w-[55%] border-white border rounded-[20px] 3xl:h-[50%]  lg:h-[80%] absolute 3xl:bottom-[20%] bottom-[10%] pl-[100px] overflow-hidden card-2 left-[calc(200vw+12.4%)] ">
+
+                  <div style={{ transform: 'rotate(-180deg)' }} className="h-full flex justify-center items-center w-full" >
+                    <div className="h-[100%] pt-[15px] left-0 w-[10%]  flex flex-col items-center">
+                      <div className="w-full h-[20%] flex items-center justify-center">
+                        <img
+                          className="w-[60px] rotate-90 relative opacity-0 top-[100px] left-[100px] top-left-image"
+                          src={data.data[1].img}
+                        />
+                      </div>
+                      <div className="h-[280px]  w-full flex justify-center overflow-hidden">
+                        <div className="w-[1.5px] bg-[#F4F4F4] h-full relative top-[100%] line-extend"></div>
+                      </div>
+                    </div>
+
+                    <div className="w-[85%] h-full flex py-[65px] ">
+                      <div className=" w-[30%] ">
+                        <h2 className="text-[44px]  pl-[5%] text-white leading-[50.2px] title">
+                          {data.data[1].content.head}
+                        </h2>
+                        <p className="mt-[16px]  pl-[5%] ">
+                          {data.data[1].content.text}
+                        </p>
+                        <button className=" pl-[5%] text-white flex gap-[13px] mt-[44px] items-center">
+                          Learn More
+                          <img src="/assets/images/right-arrow.svg" />
+                        </button>
+                      </div>
+                      <img
+                        className="w-[70%] ml-10 transform scale-x-[-1] object-cover"
+                        src={data.data[1].img3}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+
+                <div style={{ background: `url(${data.data[2].img2})`, transform: 'rotate(180deg)', backgroundSize: 'cover', }}
+                  className="w-[65%] 3xl:w-[55%] border-white border rounded-[20px] 3xl:h-[50%]  h-[80%] absolute 3xl:bottom-[20%] bottom-[10%] pl-[100px] py-[65px]  overflow-hidden card-3 left-[calc(200vw+20.4%)]">
+
+                  <div style={{ transform: 'rotate(-180deg)' }} className="h-full flex justify-center pl-9 " >
+                    <div className="pl-[5%] w-[30%] ">
+                      <h2 className="text-[44px] text-white leading-[50.2px]">
+                        {data.data[2].content.head}
+                      </h2>
+                      <p className="mt-[16px]">
+                        {data.data[2].content.text}
+                      </p>
+                      <button className="text-white flex gap-[13px] mt-[44px] items-center">
+                        Learn More
+                        <img src="/assets/images/right-arrow.svg" />
+                      </button>
+                    </div>
+                    {/* <figure> */}
+                    <img
+                      className="w-[65%] ml-14 object-cover"
+                      src={data.data[2].img3}
+                    />
+                    {/* </figure> */}
                   </div>
                 </div>
               </div>
-    
-              <div className="flex h-full relative overflow-x-hidden   w-full ">
-    
-                <div className="lg:w-[15%] justify-center items-center hidden lg:flex flex-col ">
-                  <div className="w-[50%] flex flex-col items-center justify-center text-center ">
-                    <span className="text-xl opacity-70 font-semibold ">
-                      Keep Scrolling{" "}
-                    </span>
+            </div>
+          </div>
+        </div>
 
-                      <motion.div
-                    className="relative  py-6 flex justify-center " >
-                      <motion.img
-                        className=" absolute h-[70px] opacity-80 "
-                        src="assets/images/subsidiaryPagesImages/props/Vector.png"
-                        alt=""
-                        animate={{ y:[-10, 10, -10] , transition: { duration: 2, repeat:  Infinity } }}
-                      />
-                      <motion.img
-                        className="my-3 h-[100px] "
-                        src="assets/images/subsidiaryPagesImages/props/Vector (1).png"
-                        alt=""
-                        animate={{ scale:  [1.2, 1, 1.2] , transition: { duration: 3 , repeat:  Infinity } }}
-                      />
-                    </motion.div>
-
-                    <span className="opacity-80 ">To Discover</span>
-                  </div>
-                </div>
-    
-                <div className="absolute z-50 lg:bottom-[9%] left-[18%] h-[300px] lg:h-[550px] w-full">
-                  <div className=" z-50 relative ">
-
-    
-    
-                    <div   style={{ background: `url(${data.data[0].img2})`, transform: 'rotate(180deg)',backgroundSize: 'cover', }}
-                    className="w-[65%] border-white border rounded-[20px] h-[300px] lg:h-[550px] absolute top-0 pl-[100px] overflow-hidden card-1 ">
-                      
-                      <div style={{ transform: 'rotate(-180deg)'}} className="h-full flex justify-center items-center w-full" >
-                        <div className="h-[100%] pt-[15px] left-0 w-[10%]  flex flex-col items-center">
-                          <div className="w-full h-[20%] flex items-center justify-center">
-                            <img
-                              className="w-[60px] rotate-90 relative opacity-0 top-[100px] left-[100px] top-left-image"
-                              src={data.data[0].img }
-                            />
-                          </div>
-                          <div className="h-[280px]  w-full flex justify-center overflow-hidden">
-                            <div className="w-[1.5px] bg-[#F4F4F4] h-full relative top-[100%] line-extend"></div>
-                          </div>
-                        </div>
-                        
-                        <div className="w-[85%] h-full flex py-[65px] ">
-                          <div className=" w-[30%] ">
-                            <h2 className="text-[44px]  pl-[5%] text-white leading-[50.2px] title">
-                              {data.data[0].content.head }
-                            </h2>
-                            <p className="mt-[16px]  pl-[5%] ">
-                            {data.data[0].content.text }
-                            </p>
-                            <button className=" pl-[5%] text-white flex gap-[13px] mt-[44px] items-center">
-                              Learn More
-                              <img src="/assets/images/right-arrow.svg" />
-                            </button>
-                          </div>
-                          <img
-                            className="w-[70%] ml-10 object-cover"
-                            src={data.data[0].img3}
-                          />
-                        </div>
-                      </div>
-                    </div>
-    
-    
-                    <div   style={{ background: `url(${data.data[0].img2})`, transform: 'rotate(180deg)',backgroundSize: 'cover', }}
-                     className="w-[65%] border-white border rounded-[20px] h-[300px] lg:h-[550px] absolute top-0 pl-[100px] overflow-hidden card-2 left-[calc(200vw+100px)]">
-                      
-                      <div  style={{ transform: 'rotate(-180deg)'}} className="h-full flex justify-around items-center w-full" >
-                        <div className="h-[100%] pt-[15px] left-0 w-[10%] flex flex-col items-center">
-                          <div className="w-full h-[20%] flex items-center justify-center">
-                            <img
-                              className="w-[60px] rotate-90 relative opacity-0 top-[100px] left-[100px] top-left-image"
-                              src={data.data[1].img }
-                            />
-                          </div>
-                          <div className="h-[280px]  w-full flex justify-center overflow-hidden">
-                            <div className="w-[1.5px] bg-[#F4F4F4] h-full relative top-[100%] line-extend"></div>
-                          </div>
-                        </div>
-
-                        <div className="w-[85%] h-full flex py-[65px]">
-                          <div className="w-[30%]">
-                            <h2 className="text-[44px] pl-[5%]  text-white leading-[50.2px] title">
-                            {data.data[1].content.head }
-                            </h2>
-                            <p className="mt-[16px] pl-[5%] ">
-                            {data.data[1].content.text }
-                            </p>
-                            <button className="text-white pl-[5%] flex gap-[13px] mt-[44px] items-center">
-                              Learn More
-                              <img src="/assets/images/right-arrow.svg" />
-                            </button>
-                          </div>
-                            <img
-                              className="w-[70%] ml-10 transform scale-x-[-1] object-cover"
-                              src={data.data[1].img3}
-                            />
-                        </div>
-                      </div>
-                    </div>
-    
-    
-                    <div  style={{ background: `url(${data.data[2].img2})`, transform: 'rotate(180deg)',backgroundSize: 'cover', }}
-                     className="w-[65%] border-white border rounded-[20px] h-[550px] absolute top-0 pl-[100px] py-[65px]  overflow-hidden card-3 left-[calc(200vw+200px)]">
-                      
-                      <div style={{ transform: 'rotate(-180deg)'}} className="h-full flex justify-center pl-9 " >
-                        <div className="pl-[5%] w-[30%] ">
-                          <h2 className="text-[44px] text-white leading-[50.2px]">
-                          {data.data[2].content.head }
-                          </h2>
-                          <p className="mt-[16px]">
-                          {data.data[2].content.text }
-                          </p>
-                          <button className="text-white flex gap-[13px] mt-[44px] items-center">
-                            Learn More
-                            <img src="/assets/images/right-arrow.svg" />
-                          </button>
-                        </div>
-                        {/* <figure> */}
-                          <img
-                            className="w-[65%] ml-14 object-cover"
-                            src={data.data[2].img3}
-                          />
-                        {/* </figure> */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  </div>
-
-  {/* <div className="h-[600px] w-full bg-blue-300 "></div> */}
-          {/* </div> */}
-</div>
+        {/* <div className="h-[600px] w-full bg-blue-300 "></div> */}
+        {/* </div> */}
+      </div>
     </>
   );
 };
