99dcfd2989b69fbf9d3802f914660f41993b8c71
NwobiaDavid <dnwobia@gmail.com>
2024-03-03 20:41:33 +0100

    horizontal overflow fix

--- src/sections/subsidiaryPages/SubsidiaryPageOne.tsx
+++ src/sections/subsidiaryPages/SubsidiaryPageOne.tsx
@@ -302,7 +302,7 @@ const SubsidiaryPageOne: React.FC<{
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { duration: 0.5 } }}
         exit={{ opacity: 0 }}
-        className=" "
+        className="  overflow-x-hidden "
       >
         <div
           id="hex-grid"
