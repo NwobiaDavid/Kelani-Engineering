aa04a6296ed566513509a943e63cbd424f46cc38
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-03-27 12:17:42 +0100

    add off-taker for power section

--- src/sections/SubsidiaryShowcase.tsx
+++ src/sections/SubsidiaryShowcase.tsx
@@ -101,6 +101,7 @@ const SubsidiaryShowcase = ({
   const accentColor =
     type == "engineering" ? "#E98B49" : type == "power" ? "#38B197" : "#9446A3";
   const { y } = useScrollPosition()
+  const [powerCtaHovered, setPowerCtaHovered] = useState(false);
 
   useEffect(() => {
     if (containerRef.current && setScrollTops) {
@@ -230,13 +231,13 @@ const SubsidiaryShowcase = ({
                       y: sectionReadMoreHovered ? "-99%" : 0,
                       transition: sectionReadMoreHovered
                         ? {
-                            duration: 0.3,
-                            ease: [0.43, 0.13, 0.23, 0.96],
-                          }
+                          duration: 0.3,
+                          ease: [0.43, 0.13, 0.23, 0.96],
+                        }
                         : {
-                            duration: 0.1,
-                            ease: [0.43, 0.13, 0.23, 0.96],
-                          },
+                          duration: 0.1,
+                          ease: [0.43, 0.13, 0.23, 0.96],
+                        },
                     }}
                     className=" h-full bg-white absolute right-0 -bottom-[calc(100%-1px)] left-0"
                   ></motion.div>
@@ -260,19 +261,10 @@ const SubsidiaryShowcase = ({
             </div>
           ))}
         </div>
-        {/* <div className="lg:grid lg:grid-cols-1 flex flex-col lg:grid-rows-4 lg:h-[100dvh] text-white text-[24px] lg:text-[36px] space-grotesk-medium leading-[1.25]">
-          {services?.map((service, index) => (
-            <div
-              key={index}
-              className="w-full lg:h-full flex items-center px-[40px] border-b py-[30px]"
-            >
-              {service?.title}
-            </div>
-          ))}
-        </div> */}
+
       </div>
       {/* @ts-expect-error */}
-      {!spotlightLoading && (spotlightData?.length as number) > 0 && (
+      {!spotlightLoading && ((spotlightData?.length as number) > 0 || type == "power") && (
         <div
           style={{ backgroundColor: themeColor }}
           className="relative py-[70px]"
@@ -299,6 +291,55 @@ const SubsidiaryShowcase = ({
               </div>
             </div>
             <div className="cursor-grabbing">
+              {type == "power" && <SwiperSlide>
+                <a
+                  onMouseEnter={() => setPowerCtaHovered(true)}
+                  onMouseLeave={() => setPowerCtaHovered(false)}
+                  href={"/power"}
+                  target="_blank"
+                  className="w-full rounded-[9.6px] overflow-hidden border-[#D2DADF] flex flex-col cursor-pointer relative h-[60dvh] min-h-[300px] max-h-[600px] lg:min-h-[600px]"
+                >
+                  <figure className="w-full h-full absolute">
+                    <img src="assets/images/power-plant.png" className="w-full h-full object-cover" />
+                  </figure>
+                  <div
+                    style={{ backgroundColor: themeColor, opacity: 0.4 }}
+                    className="w-full h-full top-0 bottom-0 left-0 z-[1] absolute"
+                  ></div>
+                  <div className="w-full h-full absolute top-0 right-0 left-0 bottom-0  bg-opacity-[0.4] px-[26px] lg:px-[48px] flex flex-col justify-end pb-[64px] z-30">
+                    <h4 className="relative w-fit space-grotesk-semibold text-[26px] md:text-[28px] lg:text-[40px] text-white z-30">
+                      Buy Power Now
+                      <motion.svg
+                        className="absolute -right-[40px] -top-[5px] rotate-45"
+                        initial={{ rotate: "-45deg" }}
+                        animate={{
+                          x: powerCtaHovered ? 8 : 0,
+                          y: powerCtaHovered ? -8 : 0,
+                          transition: {
+                            duration: 0.3,
+                            ease: [0.43, 0.13, 0.23, 0.96],
+                          },
+                        }}
+                        width="33"
+                        height="36"
+                        viewBox="0 0 11 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="white"
+                      >
+                        <path
+                          d="M9.64605 6.36485L6.5523 9.4586C6.45544 9.55547 6.32406 9.60989 6.18707 9.60989C6.05008 9.60989 5.9187 9.55547 5.82184 9.4586C5.72497 9.36173 5.67055 9.23036 5.67055 9.09337C5.67055 8.95638 5.72497 8.825 5.82184 8.72813L8.03516 6.51567H1.71875C1.582 6.51567 1.45085 6.46135 1.35415 6.36465C1.25745 6.26795 1.20312 6.1368 1.20312 6.00005C1.20312 5.86329 1.25745 5.73214 1.35415 5.63544C1.45085 5.53874 1.582 5.48442 1.71875 5.48442H8.03516L5.82269 3.27067C5.72583 3.1738 5.67141 3.04242 5.67141 2.90544C5.67141 2.76845 5.72583 2.63707 5.82269 2.5402C5.91956 2.44333 6.05094 2.38892 6.18793 2.38892C6.32492 2.38892 6.4563 2.44333 6.55316 2.5402L9.64691 5.63395C9.69499 5.68192 9.73312 5.73891 9.75911 5.80166C9.7851 5.8644 9.79844 5.93166 9.79836 5.99958C9.79828 6.06749 9.78478 6.13472 9.75864 6.19741C9.73251 6.26009 9.69425 6.317 9.64605 6.36485Z"
+                          fill={"white"}
+                        />
+                      </motion.svg>
+                    </h4>
+                  </div>
+                  <motion.div
+                    className="w-full h-full absolute bg-gradient-to-t from-[rgba(2,42,33,1)] to-[rgba(7,158,126,0)] top-0 right-0 left-0 bottom-0 bg-opacity-70  flex text-white items-center justify-center z-20"
+                  >
+                  </motion.div>
+                </a>
+              </SwiperSlide>
+              }
               {/* @ts-expect-error */}
               {spotlightData?.map((item: any, index: number) => (
                 <SwiperSlide key={index}>
